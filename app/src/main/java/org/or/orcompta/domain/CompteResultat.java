acd04a0362d01d8fde1945366144711d797374d7
Olivier Riverain <olivier.riverain@laposte.net>
2026-03-11 20:08:18 +0100

    compte resultat suite

--- app/src/main/java/org/or/orcompta/domain/CompteResultat.java
+++ app/src/main/java/org/or/orcompta/domain/CompteResultat.java
@@ -35,6 +35,35 @@ public class CompteResultat {
             String first = cpte.substring(0, 1);            
             switch(first) {
                 case "6":
+                    if (cpte.substring(0,3).equals("607")) compteResultat.put("FS",  compteResultat.get("FS") + debit - credit);
+                    if (cpte.substring(0,4).equals("6037")) compteResultat.put("FT",  compteResultat.get("FT") + debit - credit);
+                    if (cpte.substring(0,3).equals("601") || cpte.substring(0,3).equals("602")) compteResultat.put("FU",  compteResultat.get("FU") + debit - credit);
+                    if (cpte.substring(0,4).equals("6031") || cpte.substring(0,4).equals("6032")) compteResultat.put("FV",  compteResultat.get("FV") + debit - credit);
+                    if (cpte.substring(0,3).equals("604") || cpte.substring(0,3).equals("605") || cpte.substring(0,3).equals("606") || cpte.substring(0,2).equals("61") || cpte.substring(0,2).equals("62")) compteResultat.put("FW",  compteResultat.get("FW") + debit - credit);
+                    if (cpte.substring(0,2).equals("63")) compteResultat.put("FX",  compteResultat.get("FX") + debit - credit);
+                    if (cpte.substring(0,3).equals("641") || cpte.substring(0,3).equals("644")) compteResultat.put("FY",  compteResultat.get("FY") + debit - credit);
+                    if (cpte.substring(0,3).equals("645") || cpte.substring(0,3).equals("646") || cpte.substring(0,3).equals("647") || cpte.substring(0,3).equals("648")) compteResultat.put("FZ",  compteResultat.get("FZ") + debit - credit);
+
+
+
+
+
+
+
+
+//if (substr($_,0,4) eq "6811")   { $cr {GA} {montant} += $balance {$_} {montant_debit} - $balance {$_} {montant_credit} ; }
+
+//if (substr($_,0,4) eq "6816")   { $cr {GB} {montant} += $balance {$_} {montant_debit} - $balance {$_} {montant_credit} ; }
+
+//if (substr($_,0,4) eq "6817")   { $cr {GC} {montant} += $balance {$_} {montant_debit} - $balance {$_} {montant_credit} ; }
+
+//if (substr($_,0,4) eq "6815")   { $cr {GD} {montant} += $balance {$_} {montant_debit} - $balance {$_} {montant_credit} ; }
+
+//if ( (substr($_,0,3) eq "651") || (substr($_,0,3) eq "653") || (substr($_,0,3) eq "654") || (substr($_,0,3) eq "658") || (substr($_,0,4) eq "6812" ) )   { $cr {GE} {montant} += $balance {$_} {montant_debit} - $balance {$_} {montant_credit} ; }
+
+//if (substr($_,0,4) eq "6551")   { $cr {GH} {montant} += $balance {$_} {montant_credit} - $balance {$_} {montant_debit} ; }
+
+//if (substr($_,0,4) eq "6555")   { $cr {GI} {montant} += $balance {$_} {montant_debit} - $balance {$_} {montant_credit} ; }
 
 
                 break;
